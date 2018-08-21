1129c35c55b3d2f1a86432e4ec757c1dbfc4ef7e
Neg <serg.zorg@gmail.com>
2018-08-21 15:30:26 +0300

    [bin/scripts] compare images cleanup

--- scripts/compare_images.py
+++ scripts/compare_images.py
@@ -1,10 +1,9 @@
 #!/usr/sbin/python
 from PIL import Image
-import imagehash
+from imagehash import dhash
 import sys
 
-h1 = imagehash.dhash(Image.open(sys.argv[1]))
-h2 = imagehash.dhash(Image.open(sys.argv[2]))
-
-print(h1 == h2)
+print(
+    dhash(Image.open(sys.argv[1])) == dhash(Image.open(sys.argv[2]))
+)
 
