8055554c28279b629ec7d86e1da9d2a4f33b8b5b
Neg <serg.zorg@gmail.com>
2019-02-14 13:01:42 +0300

    [bin/scripts] start polybar with env vars

---
+++ scripts/panel_run.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+export POLYBAR_INPUT_FORMAT="input:%{O5}%layout%"
+
+export POLYBAR_MPD_FORMAT_PAUSED="%{F#395573}⟬%{O5}%{F#005f87}paused%{F#395573} %{F#395573}⟭" 
+export POLYBAR_MPD_LEFT_SIDE="%{F#395573}⟬%{O5}%{F#005f87}〉%{F#005fd7}〉%{F#395573} %{F#ccc}"
+export POLYBAR_MPD_SONG_TIME="%elapsed%%{F#395573}/%{F#ccc}%total%"
+export POLYBAR_MPD_OFFLINE="%{F#395573}⟬%{O5}%{F#ccc}No MPD%{O5}%{F#395573}⟭ %{F#ccc}"
+export POLYBAR_MPD_FORMAT_ONLINE="<label-song> <label-time>"
+
+export POLYBAR_LL="%{F#395573}⟬%{F#ccc}"
+export POLYBAR_RR="%{O5}%{F#395573}⟭%{F#ccc}"
+
+if [[ $(pgrep polybar|wc -l) -le 1  ]]; then
+    polybar -c ${XDG_CONFIG_HOME}/polybar/main main &
+fi
