d92422cabaf8bd03fd74735aba24f8bdd92189b2
Neg <serg.zorg@gmail.com>
2019-03-28 14:04:52 +0300

    [bin/scripts] move panel_run to i3_prepare

--- scripts/i3_prepare.sh
+++ scripts/i3_prepare.sh
@@ -2,5 +2,5 @@
 make -C ${XDG_CONFIG_HOME}/i3/ &
 ${XDG_CONFIG_HOME}/i3/negi3mods.py >> ${HOME}/tmp/negi3mods.log 2>&1 &
 pkill -f 'mpc idle'
-pkill sxhkd
-sxhkd &
+pkill sxhkd; sxhkd &
+~/bin/scripts/panel_run.sh hard
