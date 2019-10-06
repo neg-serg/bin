72195b7ec1df45ecea11796de0c2a77d79f6c6e5
Neg <serg.zorg@gmail.com>
2019-10-07 00:00:36 +0300

    [bin/scripts] remove panel_run from i3_prepare to i3 config

--- scripts/i3_prepare.sh
+++ scripts/i3_prepare.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
+${XDG_CONFIG_HOME}/i3/negi3mods.py --start >> ${HOME}/tmp/negi3mods.log 2>&1 &
 make -C ${XDG_CONFIG_HOME}/i3/ &
-${XDG_CONFIG_HOME}/i3/negi3mods.py >> ${HOME}/tmp/negi3mods.log 2>&1 &
 pkill -f 'mpc idle'
 pkill sxhkd; sxhkd &
-~/bin/scripts/panel_run.sh hard
